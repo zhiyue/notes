646f802700ecb26c1a1c2981c1eb049a5b8fd325
dirtysalt <dirtysalt1987@gmail.com>
2014-08-07 07:56:22 +0800

    aggregate into one Vagrantfile

--- files/.vm.provision.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-#Copyright (C) dirlt
-
-sudo apt-get install -y zsh
-chsh -s /bin/zsh
-sudo apt-get install -y git
-sudo apt-get install -y libaio-dev
-sudo apt-get install -y mysql-client
-
